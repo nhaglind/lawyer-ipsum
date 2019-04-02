28bf1543cde61ad9923094c018a12ed13744d34d
Nick Haglind <nhaglind@gmail.com>
2019-04-02 14:54:12 -0500

    add proc for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./router');
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(routes);
